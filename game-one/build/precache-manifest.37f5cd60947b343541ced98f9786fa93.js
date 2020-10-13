f500e49984021d47c2eb378fde4cb3a2c7f4b656
Asapanna Rakesh <rakeshark22@gmail.com>
2020-10-13 14:05:19 +0530

    build dir add, remove workflow

---
+++ game-one/build/precache-manifest.37f5cd60947b343541ced98f9786fa93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6289989e8c55fa5287455ef9e8366ab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa9b0cd31cdf6ea477b1",
+    "url": "/static/css/main.898ec8da.chunk.css"
+  },
+  {
+    "revision": "28d2c33bf028e980a9cf",
+    "url": "/static/js/2.6862140c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6862140c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa9b0cd31cdf6ea477b1",
+    "url": "/static/js/main.e8ff716a.chunk.js"
+  },
+  {
+    "revision": "2b8c6547b2eb1b0aeab7",
+    "url": "/static/js/runtime-main.f148288b.js"
+  }
+]);
